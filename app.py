2c79b57089cc2ae5a28fc2d1b2d5dc9a029d1249
moli-debugger <mkmohamedalimoli@gmail.com>
2021-07-09 15:36:12 +0530

    v 1.2

--- app.py
+++ app.py
@@ -6,44 +6,47 @@ db=firebase.database()
 #Global Varible
 global i
 i=0
-selected_Value=[]
+points=0
 
 data=db.child('quizz').child('questions').get()
-def next_Question():
-    global i
-    i=i+1
-    next_Data=data.val()[i]
-    print(next_Data)
-    return 'pooda'
+
 @app.route('/')
 def hello_world():
     try:
         global i
-        print('2 hello',i)
         q=data.val()[i]
         question=q['question']
         option_a=q['answers'][0]
         option_b=q['answers'][1]
         option_c=q['answers'][2]
         option_d=q['answers'][3]
-        print(q)
         return render_template('index.html',Question=question,Option_a=option_a,Option_b=option_b,Option_c=option_c,Option_d=option_d)
     except:
+        print(Exception)
         return "thank you"
-@app.route('/action-submit')
+@app.route('/action-submit',methods=['POST','GET'])
 def submit():
     try:
-        global i
-        i=i+1
-        print(i)
+        if request.method=='GET':
+            global i
+            selectedValue=request.args.get('answer')
+            index=data.val()[i]['correctIndex']
+            test=data.val()[i]['answers'][index]
+            print(test)
+            if(selectedValue==data.val()[i]['answers'][index]):
+                global points
+                points=points+1
+                print(points,'points')
+            i=i+1
         return hello_world()
     except:
         print('thank you')
 
 
-@app.route('/<selectedValue>', methods=['POST'])
+
 def click():
-    selectedValue = request.form['option']
-    return print(selected_Value.append(selectedValue))
+
+    print('click')
+
 if __name__ == '__main__':
     app.run()
